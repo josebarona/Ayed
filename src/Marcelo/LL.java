2b931f1391450bf75c4d220141b1d52b398f1e49
josebarona <jose.barona@ing.austral.edu.ar>
2019-02-02 22:09:30 -0500

    insert before and after

--- src/Marcelo/LL.java
+++ src/Marcelo/LL.java
@@ -1,5 +1,7 @@
 package Marcelo;
 
+@SuppressWarnings("all")
+
 public class LL<T>{
     private Node<T> first ;
     private int size ;
@@ -14,10 +16,52 @@ public class LL<T>{
             Node<T> temp = this.first ;
             this.first = new Node<>(elem,temp) ;
         }
+        this.size++ ;
+    }
+
+    public void insertBefore(int index, T elem){
+        Node<T> current = this.first ;
+        int i = 0 ;
+        while (i<index-1){
+            current = current.next ;
+            i++ ;
+        }
+        Node<T> temp = current.next ;
+        current.next = new Node<T>(elem,temp) ;
+        this.size++ ;
+    }
+
+    public void insertAfter(int index, T elem){
+        Node<T> current = this.first ;
+        int i = 0 ;
+        while (i<index){
+            current = current.next ;
+            i++ ;
+        }
+        Node<T> temp = current.next ;
+        current.next = new Node<>(elem,temp) ;
+        this.size++ ;
+    }
+
+    public void delete(int index){
+        Node<T> current = this.first ;
+        for(int i=0 ; i<index-1 ; i++) current = current.next ;
+        current.next = current.next.next ;
+        this.size-- ;
     }
 
     public void print(){
         this.first.show() ;
+        System.out.println();
+    }
+
+    public void printWithIndex(){
+        Node<T> current = this.first ;
+        for(int i=0 ; i<size ; i++){
+            System.out.print(i + ": " + current.elem + " , ") ;
+            current = current.next ;
+        }
+        System.out.println();
     }
 
     private class Node<T>{
@@ -51,7 +95,13 @@ public class LL<T>{
         list.add("leonardo") ;
         list.add("victor hugo") ;
         list.add("sol") ;
-        list.print();
+        list.printWithIndex();
+        list.insertBefore(3,"harry") ;
+        list.printWithIndex();
+        list.insertAfter(4,"hermione") ;
+        list.printWithIndex();
+        list.delete(10) ;
+        list.printWithIndex();
     }
 
 }
